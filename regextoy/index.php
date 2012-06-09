b48bc920675cc55f9f679ef27937fab22d65189f
Pathoschild <pathoschild+github@gmail.com>
2012-06-09 15:57:00 -0400

    updated script.

--- regextoy/index.php
+++ regextoy/index.php
@@ -2,7 +2,6 @@
 require_once( '../backend/modules/Backend.php' );
 $backend = Backend::create('Regex toy', '')
 	->link( 'stylesheet.css' )
-	->link( 'http://pathoschild.github.com/Wikimedia-contrib/pathoschild.regexeditor.css' )
 	->link( 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js' )
 	->link( 'https://raw.github.com/Pathoschild/Wikimedia-contrib/master/pathoschild.regexeditor.js' )
 	->addScript('
