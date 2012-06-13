c508202458b7b809d8b890b4ead575b3a8f56cfd
Jesse Plamondon-Willard <pathoschild+github@gmail.com>
2012-06-13 01:08:43 +0000

    + global notice about server issues

---
+++ backend/notice.php
@@ -0,0 +1,13 @@
+<?
+if($enabled = true) {
+	/* configure */
+	$type = 'notice';
+	$message = 'The Wikimedia Toolserver (which hosts this tool) is having hardware load issues. You might experience temporary errors or timeouts.';
+	
+	#$type = 'error';
+	#$message = 'The Wikimedia Toolserver is experiencing <a href="https://jira.toolserver.org/browse/TS-1309" title="Toolserver bug ticket: daphne s2/s5 corrupted and offline">database server failures</a>. Some tools may be unusable.';
+	
+	/* render */
+	echo '<div id="global-notice" class="is-', $type, '">', $message, '</div>';
+}
+?>
