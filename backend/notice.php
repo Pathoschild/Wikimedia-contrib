a61cbd743d2a298f6e9e9980248c1a5a31550d3b
Jesse Plamondon-Willard <pathoschild+github@gmail.com>
2013-01-06 23:10:18 +0000

    accounteligibility: added 2013 steward elections

--- backend/notice.php
+++ backend/notice.php
@@ -1,8 +1,8 @@
 <?
 if($enabled = false) {
 	/* configure */
-	$type = 'warn';
-	$message = 'The Wikimedia Toolserver (which hosts this tool) is having hardware load issues. You might experience temporary errors or timeouts.';
+	$type = 'error';
+	$message = 'This tool may fail &mdash; the Wikimedia Toolserver (which hosts this tool) is experiencing database replication problems.';
 	
 	#$type = 'error';
 	#$message = 'The Wikimedia Toolserver is experiencing <a href="https://jira.toolserver.org/browse/TS-1309" title="Toolserver bug ticket: daphne s2/s5 corrupted and offline">database server failures</a>. Some tools may be unusable.';
