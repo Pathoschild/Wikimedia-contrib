1a5169104b3b82ba5e695754331b257e55d8e174
Jesse Plamondon-Willard <pathoschild+github@gmail.com>
2013-02-22 04:35:15 +0000

    workaround: disable access to wikidata during outage

--- backend/notice.php
+++ backend/notice.php
@@ -1,8 +1,8 @@
 <?
-if($enabled = false) {
+if($enabled = true) {
 	/* configure */
-	$type = 'error';
-	$message = 'This tool may fail &mdash; the Wikimedia Toolserver (which hosts this tool) is experiencing database replication problems.';
+	$type = 'warn';
+	$message = 'The Wikidata database is temporarily unavailable as the toolserver adjusts to breaking database changes.';
 	
 	#$type = 'error';
 	#$message = 'The Wikimedia Toolserver is experiencing <a href="https://jira.toolserver.org/browse/TS-1309" title="Toolserver bug ticket: daphne s2/s5 corrupted and offline">database server failures</a>. Some tools may be unusable.';
