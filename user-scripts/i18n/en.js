fdfa100bf7105c5364878dd7565ab487a2e6fa6b
Jesse Plamondon-Willard <github@jplamondonw.com>
2015-08-15 00:35:37 -0400

    added en + fr user script translation files

---
+++ user-scripts/i18n/en.js
@@ -0,0 +1,39 @@
+/**
+ * This is a sample translation file for these scripts. See https://meta.wikimedia.org/wiki/TemplateScript#Translation
+ * for instructions on using translation files.
+ * 
+ * To translate these scripts:
+ *   1. Copy this to a *new* file named with your language code.
+ *   2. Translate the text inside 'quotes'. Be careful with the special tokens that look like
+ *      {variable}; these are placeholders. Only translate the text right of the '=' symbol. For example,
+ *      only "reset the form" should be translated for the "{resetForm|text=reset the form}" token.
+ *   3. Submit a pull request to the Git repository, or simply post it to https://meta.wikimedia.org/wiki/User_talk:Pathoschild
+ *      and I'll do the rest.
+ */
+
+var pathoschild = pathoschild || {};
+pathoschild.i18n = {
+	templatescript: {
+		defaultHeaderText: 'TemplateScript', // the sidebar header text label for the default group
+		regexEditor: 'Regex editor' // the default 'regex editor' script
+	},
+	regexeditor: {
+		header: 'Regex editor', // the header text shown in the form
+		search: 'Search',       // the search input label
+		replace: 'Replace',     // the replace input label
+		nameSession: 'Enter a name for this session', // the prompt shown when saving the session
+		loadSession: 'Load session "{name}"',         // tooltip shown for a saved session, where {name} is replaced with the session name
+		deleteSession: 'Delete session "{name}"',     // tooltip shown for the delete icon on a saved session, where {name} is replaced with the session name
+		launchConflict: 'You are launching the regex editor tool, but it\'s already open. Do you want to {resetForm|text=reset the form} or {cancelForm|text=cancel the form}?', // the message shown when relaunching the form, where {resetForm} and {cancelForm} will be replaced with appropriate form elements.
+		closeEditor: 'Close the regex editor',        // tooltip shown for the close-editor icon
+		addPatterns: 'add patterns',                  // button text
+		addPatternsTooltip: 'Add search & replace boxes', // button tooltip
+		apply: 'apply',                               // button text
+		applyTooltip: 'Perform the above patterns',   // button tooltip
+		undo: 'undo the last apply',                  // button text
+		undoTooltip: 'Undo the last apply',           // button tooltip
+		save: 'save',                                 // button text
+		saveTooltip: 'Save this session for later use', // button tooltip
+		instructions: 'Enter any number of regular expressions to execute. The search pattern can be like "{code|text=search pattern}" or "{code|text=/pattern/modifiers}", and the replace pattern can contain reference groups like "{code|text=$1}" (see {helplink|text=tutorial|title=JavaScript regex tutorial|url=http://www.regular-expressions.info/javascript.html}).'
+	}
+};
---
+++ user-scripts/i18n/fr.js
@@ -0,0 +1,31 @@
+/**
+ * This is a translation file for these scripts. See https://meta.wikimedia.org/wiki/TemplateScript#Translation
+ * for instructions on using translation files.
+ */
+
+var pathoschild = pathoschild || {};
+pathoschild.i18n = {
+	templatescript: {
+		defaultHeaderText: 'TemplateScript', // the sidebar header text label for the default group
+		regexEditor: 'éditeur regex' // the default 'regex editor' script
+	},
+	regexeditor: {
+		header: 'éditeur regex',     // the header text shown in the form
+		search: 'Recherche',         // the search input label
+		replace: 'Remplacement',     // the replace input label
+		nameSession: 'Entrez un nom pour cette session', // the prompt shown when saving the session
+		loadSession: 'Charger la session "{name}"',      // tooltip shown for a saved session, where {name} is replaced with the session name
+		deleteSession: 'Supprimer la session "{name}"',  // tooltip shown for the delete icon on a saved session, where {name} is replaced with the session name
+		launchConflict: 'Vous lancez l\'outil de l\'éditeur d\'expressions régulières, mais il est déjà ouvert. Voulez-vous {resetForm|texte=réinitialiser l\'outil} ou {cancelForm|texte=annuler la réouverture}?', // the message shown when relaunching the form, where {resetForm} and {cancelForm} will be replaced with appropriate form elements.
+		closeEditor: 'Fermer l\'éditeur de regex',      // tooltip shown for the close-editor icon
+		addPatterns: 'ajouter des motifs',              // button text
+		addPatternsTooltip: 'Ajouter des motifs',       // button tooltip
+		apply: 'appliquer',                             // button text
+		applyTooltip: 'Effectuer les motifs ci-dessus', // button tooltip
+		undo: 'annuler la dernière application',        // button text
+		undoTooltip: 'Annuler la dernière application', // button tooltip
+		save: 'sauvegarder',                            // button text
+		saveTooltip: 'Enregistrer cette session pour une utilisation ultérieure', // button tooltip
+		instructions: 'Entrez un nombre quelconque d\'expressions régulières à exécuter. Le motif de recherche peut être « {code|text=texte simple} » ou un motif regex tel que « {code|text=/motif/modificateurs} », et le remplacement peut contenir des groupes de référence tel que « {code|text=$1} » (voir {helplink|text=un tutoriel (en anglais)|title=tutoriel de regex JavaScript|url=http://www.regular-expressions.info/javascript.html}).'
+	}
+};
