ad7e92def9ada101ef42238ae24854fec80cc539
Jesse Plamondon-Willard <github@jplamondonw.com>
2015-08-17 00:25:40 -0400

    ajax transclusion table: rewrite & migrate to module pattern

--- user-scripts/pathoschild.ajaxtransclusiontable.js
+++ user-scripts/pathoschild.ajaxtransclusiontable.js
@@ -1,140 +1,111 @@
-// <pre><nowiki>
-/*************
-*** AJAX transclusion table <http://meta.wikimedia.org/wiki/User:Pathoschild/Scripts/AJAX_transclusion_table>
-*** by [[m:user:Pathoschild]]
-*************/
- 
-// Documentation:
-//    - uses tables with class="attable"
-function attIntialize() {
-	/* namespace */
-	var att = {};
- 
-	/* find AJAX tables */
-	att.alltables = document.getElementsByTagName('table'); // all tables
-	att.tables = new Array(); // AJAX tables
-	att.count = att.alltables.length; // count all tables
-	var x = 0; // count AJAX tables
-	for(var i=0; i<att.count; i++) {
-		if(att.alltables[i].getAttribute('class')) {
-			if(att.alltables[i].getAttribute('class').match(/\battable\b/)) {
-				att.tables[x] = att.alltables[i];
-				x++;
-			}
-		}
-	}
-	if(att.tables.length) {
-		/* array of all att rows */
-		att.rows = new Array;
- 
-		/* rebuild tables with tbodies */
-		att.countTables = att.tables.length;
-		for(var i=0; i<att.countTables; i++) {
-			/* get elements */
-			att.table = att.tables[i];
-			att.trows = att.table.getElementsByTagName('tr');
-			att.countRows = att.trows.length;
- 
-			/* rebuild and fill row array */
-			att.tbody = document.createElement('tbody');
-			for(var x=0; x<att.countRows; x++) {
-				att.countTemp = att.rows.length;
-				att.rows[att.countTemp] = att.trows[x].cloneNode(true);
-				att.rows[att.countTemp].setAttribute('id','oldrow_'+att.countTemp);
-				att.tbody.appendChild(att.rows[att.countTemp]);
-			}
-			att.table.innerHTML = '';
-			att.table.appendChild(att.tbody);
-		}
- 
-		/* create template add/show link */
-		att.shlink = document.createElement('a');
-		att.shlink.setAttribute('style','font-size:0.9em; cursor:pointer;');
-		att.shlink.appendChild(document.createTextNode('[show] '));
- 
-		/* add show/hide links */
-		att.countRows = att.rows.length;
-		for(var i=0; i<att.countRows; i++) {
-			att.row = att.rows[i];
-			att.firstCell = att.row.getElementsByTagName('td')[0];
-			// skip rows with no links or cells
-			att.link = att.row.getElementsByTagName('a')[0];
-			if(att.link && att.link.getAttribute('href').match(/^\/wiki/) && att.firstCell) {
-				att.row.setAttribute('id','oldrow_'+i);
-				att.newlink = att.shlink.cloneNode(true);
-				att.newlink.setAttribute('OnClick','attShow('+i+');');
-				att.newlink.setAttribute('id','attlink_'+i);
-				att.firstCell.insertBefore(att.newlink,att.firstCell.firstChild);
-			}
-		}
-	}
-}
- 
-function attShow(id) {
-	/* get elements */
-	var att = {};	
-	att.row = document.getElementById('oldrow_'+id);
-	att.tbody = att.row.parentNode;
- 
-	/* build display row */	
-	att.colspan = att.row.getElementsByTagName('td').length;
-	att.newrow = document.createElement('tr');
-	att.newrow.setAttribute('id','newrow_'+id);
-	att.newcell = document.createElement('td');
-	att.newcell.setAttribute('colspan',att.colspan);
-	att.newbox = document.createElement('div');
-	att.newbox.setAttribute('style','margin:0.5em; padding:0.5em; border:2px solid gray;');
-	att.loader = document.createElement('img');
-	att.loader.setAttribute('src','//upload.wikimedia.org/wikipedia/commons/d/d2/Spinning_wheel_throbber.gif');
-	att.newbox.appendChild(att.loader);
-	att.newcell.appendChild(att.newbox);
-	att.newrow.appendChild(att.newcell);
-	att.tbody.insertBefore(att.newrow,att.row.nextSibling);
- 
-	/* get title */
-	att.links = att.row.getElementsByTagName('a');
-	att.count = att.links.length;
-	for(var i=0; i<att.count; i++) {
-		if(att.links[i].getAttribute('title')) {
-			att.title = att.links[i].getAttribute('title');
-			break;
-		}
-	}
-	att.query = sajax_init_object();
-	att.url = wgServer+'/wiki/'+att.title+'?action=render';
-	att.query.open('GET',att.url,true);
-	att.query.send('');
-	att.query.onreadystatechange = function() {
-		if(att.query.readyState==4) {
-			if(att.query.status==200) {
-				att.newbox.innerHTML = att.query.responseText;
+/*
+
+
+The ajax transclusion table script adds a "[show]" link in the first cell of every row in tables
+with the "attable" class. Clicking the link will display the linked page below. No show/hide link
+will be shown in rows that don't contain a link, like headings.
+
+
+*/
+/* global $, mw */
+/* jshint eqeqeq: true, latedef: true, nocomma: true, undef: true */
+var pathoschild = pathoschild || {};
+
+$(function() {
+	'use strict';
+
+	if (pathoschild.ajaxTransclusionTables)
+		return; // already initialised, don't overwrite
+
+
+	/**
+	 * Singleton responsible for handling ajax transclusion tables.
+	 * @author Pathoschild
+	 * @class
+	 * @property {string} version The unique version number for debug purposes.
+	 */
+	pathoschild.ajaxTransclusionTables = (function() {
+		var self = {};
+
+		/*********
+		** Fields
+		*********/
+		self.version = '0.2';
+
+
+		/*********
+		** Private methods
+		*********/
+		var _toggle = function() {
+			// read toggle
+			var toggle = $(this);
+			var data = toggle.data();
+
+			// toggle transclusion
+			if(data.expanded) {
+				$(data.container).remove();
+				toggle.text('[show] ').data({ expanded: false, container: null });
+				return;
 			}
 			else {
-				att.loader.setAttribute('img','//upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Achtung.svg/32px-Achtung.svg.png');
+				// get row details
+				var oldRow = toggle.closest('tr');
+				var rowID = 'att-' + (new Date()).getTime();
+				var colspan = oldRow.find('> td').length;
+
+				// update UI
+				toggle.text('[hide] ').data({ expanded: true, container: '#' + rowID });
+				var newDiv = $('<div class="att-container">').appendTo(
+					$('<td>').attr('colspan', colspan).appendTo(
+						$('<tr>').attr('id', rowID).insertAfter(oldRow)
+					)
+				);
+				newDiv.append($('<img>').attr('src', '//upload.wikimedia.org/wikipedia/commons/d/d2/Spinning_wheel_throbber.gif'));
+
+				$.ajax(mw.config.get('wgServer') + '/wiki/' + data.title + '?action=render').then(function(data) {
+					newDiv.html(data);
+				});
 			}
-		}
-	}
-	/* update show/hide link */
-	att.shlink = document.getElementById('attlink_'+id);
-	att.shlink.innerHTML = '[hide] ';
-	att.shlink.setAttribute('OnClick','attHide('+id+');');
-}
- 
-function attHide(id) {
-	/* get elements */
-	var att = {};
-	att.row = document.getElementById('newrow_'+[id]);
-	att.shlink = document.getElementById('attlink_'+id);
- 
-	/* remove display row */
-	att.row.parentNode.removeChild(att.row);
- 
-	/* update show/hide link */
-	att.shlink.innerHTML = '[show] ';
-	att.shlink.setAttribute('OnClick','attShow('+id+');');
-}
- 
-if(!location.href.match('disableatt=true')) {
-	attIntialize();
-}
-// </nowiki></pre>
+		};
+
+
+		/*********
+		** Public methods
+		*********/
+		/**
+		 * Bootstrap and hook into the UI. This method should only be called once the DOM is ready.
+		 */
+		self.initialise = function() {
+			// find cells to inject
+			var rows = $('table.attable tr');
+			if(!rows.length)
+				return;
+
+			// add styles
+			mw.util.addCSS(
+				  '.att-container { margin:0.5em; padding:0.5em; border:2px solid gray; }'
+				+ '.att-toggle { font-size:0.9em; cursor:pointer; }'
+			);
+
+			// inject links
+			var toggle = $('<a>').addClass('att-toggle').text('[show] ');
+			rows.each(function(i, row) {
+				// get title to transclude
+				row = $(row);
+				var cell = row.find('td:first');
+				var link = cell.find('a:first');
+				var title = link.attr('title');
+				if(!link.length || !link.attr('href').match(/^\/wiki/) || !title)
+					return;
+
+				// inject toggle
+				cell.prepend(
+					toggle.clone().data({ title: title, expanded: false }).click(_toggle)
+				);
+			});
+		};
+
+		$(self.initialise);
+		return self;
+	})();
+});
