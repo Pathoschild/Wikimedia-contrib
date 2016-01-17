70f8d5127a8a14d0648dfa8b854070e2b14d52a3
Jesse Plamondon-Willard <github@jplamondonw.com>
2016-01-17 10:45:04 -0500

    templatescript: add Russian translations (thanks to KPu3uC B Poccuu)

---
+++ user-scripts/i18n/ru.js
@@ -0,0 +1,31 @@
+/**
+ * This is a translation file for these scripts. See https://meta.wikimedia.org/wiki/TemplateScript#Translation
+ * for instructions on using translation files.
+ */
+
+var pathoschild = pathoschild || {};
+pathoschild.i18n = {
+	templatescript: {
+		defaultHeaderText: 'TemplateScript', // the sidebar header text label for the default group
+		regexEditor: 'Редактор рег. выраж.'  // the default 'regex editor' script
+	},
+	regexeditor: {
+		header: 'Редактор регулярных выражений',   // the header text shown in the form
+		search: 'Поиск',                           // the search input label
+		replace: 'Замена',                         // the replace input label
+		nameSession: 'Укажите имя данной сессии',  // the prompt shown when saving the session
+		loadSession: 'Загрузить сессию «{name}»',  // tooltip shown for a saved session, where {name} is replaced with the session name
+		deleteSession: 'Удалить сессию «{name}»',  // tooltip shown for the delete icon on a saved session, where {name} is replaced with the session name
+		closeEditor: 'Закрыть редактор',           // tooltip shown for the close-editor icon
+		addPatterns: 'Добавить шаблон',            // button text
+		addPatternsTooltip: 'Добавить поле поиска и замены', // button tooltip
+		apply: 'Применить',                        // button text
+		applyTooltip: 'Выполнить данные шаблоны',  // button tooltip
+		undo: 'Отмена последнего действия',        // button text
+		undoTooltip: 'Отмена последнего действия', // button tooltip
+		save: 'Сохранить',                         // button text
+		saveTooltip: 'Сохранение сессии для применения в будущем', // button tooltip
+		instructions: 'Вы можете указать любое количество шаблонов для использования. Шаблон поиска может выглядеть как «{code|text=шаблон поиска}»\
+ , либо как «{code|text=/шаблон/флаги}», а шаблон замены может содержать ссылки на группы вида «{code|text=$1}» (см. {helplink|text=tutorial|title=JavaScript regex tutorial|url=http://www.regular-expressions.info/javascript.html}).'
+	}
+};
