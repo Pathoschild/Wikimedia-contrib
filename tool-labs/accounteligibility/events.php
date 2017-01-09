605e27ceb107264b5a8fc40c30d64b5cbfaa1a30
Jesse Plamondon-Willard <github@jplamondonw.com>
2017-01-09 16:05:08 -0500

    add 2017 steward elections

--- tool-labs/accounteligibility/events.php
+++ tool-labs/accounteligibility/events.php
@@ -14,6 +14,29 @@ class EventFactory
     public function getEvents()
     {
         ##########
+        ## 2017: steward elections
+        ##########
+        // voters
+        yield (new Event(41, 2017, 'steward elections', '//meta.wikimedia.org/wiki/Stewards/Elections_2017'))
+            ->addRule(new NotBotRule(), Workflow::HARD_FAIL)
+            ->addRule(new EditCountRule(600, null, '<201611', EditCountRule::ACCUMULATE))// 600 edits before 01 November 2016
+            ->addRule(new EditCountRule(50, '201608', '<201702', EditCountRule::ACCUMULATE));// 50 edits between 01 August 2016 and 31 January 2017
+
+        // candidates
+        yield (new Event(40, 2017, 'steward elections (candidates)', '//meta.wikimedia.org/wiki/Stewards/Elections_2017'))
+            ->addRule(new NotBotRule(), Workflow::HARD_FAIL)
+            ->addRule(new EditCountRule(600, null, '<201611', EditCountRule::ACCUMULATE))// 600 edits before 01 November 2016
+            ->addRule(new EditCountRule(50, '201608', '<201702', EditCountRule::ACCUMULATE))// 50 edits between 01 August 2016 and 31 January 2017
+            ->addRule(new HasGroupDurationRule('sysop', 90, '<201702'), Workflow::ON_ANY_WIKI)// flagged as a sysop for three months
+            ->withAction('<strong>be a candidate</strong>')
+            ->withExtraRequirements([
+                'You must be 18 years old, and at least the age of majority in your country before the final day of voting.',
+                'You must agree to abide by the policies governing <a href="//meta.wikimedia.org/wiki/Stewards_policy" title="Steward policy">steward access</a>, <a href="//meta.wikimedia.org/wiki/CheckUser_policy" title="checkuser policy">checkuser access</a>, <a href="//meta.wikimedia.org/wiki/Oversight_policy" title="oversight policy">oversight access</a>, and <a href="//wikimediafoundation.org/wiki/privacy_policy" title="privacy policy">privacy</a>.',
+                'You must <a href="https://meta.wikimedia.org/wiki/Special:MyLanguage/Access_to_nonpublic_information_policy" title="Access to nonpublic information policy">sign the confidentiality agreement</a>.'
+            ]);
+
+
+        ##########
         ## 2016: Commons Picture of the Year for 2015
         ##########
         yield (new Event(39, 2016, 'Commons Picture of the Year for 2015', '//commons.wikimedia.org/wiki/Commons:Picture_of_the_Year/2015'))
