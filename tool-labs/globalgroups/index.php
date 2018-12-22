33c2c6873716050f3ea59338bf5058a136cad4a7
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2018-12-22 14:03:52 -0500

    fix typo

--- tool-labs/globalgroups/index.php
+++ tool-labs/globalgroups/index.php
@@ -162,7 +162,7 @@ $flagBlurbs = [
 
     // OATHAuth extension: https://github.com/wikimedia/mediawiki-extensions-OATHAuth/blob/master/i18n/en.json
     "right-oathauth-api-all" => "Query and validate OATH information for self and others",
-    "right-oathauth-disable-for-user": "Disable two-factor authentication for a user",
+    "right-oathauth-disable-for-user" => "Disable two-factor authentication for a user",
     "right-oathauth-enable" => "Enable two-factor authentication",
 
     // OAuth extension: https://github.com/wikimedia/mediawiki-extensions-OAuth/blob/master/i18n/en.json
