8641fa0fb1708af5374b50d8dc26b11b4b6faac0
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-11-17 20:41:08 -0500

    add 2020 steward elections (#135)

--- tool-labs/accounteligibility/framework/EventFactory.php
+++ tool-labs/accounteligibility/framework/EventFactory.php
@@ -14,7 +14,7 @@ class EventFactory
      */
     public function getDefaultEventID()
     {
-        return 50;
+        return 52;
     }
 
     /**
@@ -22,6 +22,28 @@ class EventFactory
      */
     public function getEvents()
     {
+        ##########
+        ## 2020: steward elections
+        ##########
+        // voters
+        yield (new Event(52, 2020, 'steward elections', '//meta.wikimedia.org/wiki/Stewards/Elections_2020'))
+            ->addRule(new NotBotRule(), Workflow::HARD_FAIL)
+            ->addRule(new EditCountRule(600, null, '<201911', EditCountRule::ACCUMULATE))// 600 edits before 01 November 2019
+            ->addRule(new EditCountRule(50, '201908', '<202002', EditCountRule::ACCUMULATE));// 50 edits between 01 August 2019 and 31 January 2020
+
+        // candidates
+        yield (new Event(51, 2020, 'steward elections (candidates)', '//meta.wikimedia.org/wiki/Stewards/Elections_2020'))
+            ->addRule(new NotBotRule(), Workflow::HARD_FAIL)
+            ->addRule(new EditCountRule(600, null, '<201911', EditCountRule::ACCUMULATE))// 600 edits before 01 November 2019
+            ->addRule(new EditCountRule(50, '201908', '<202002', EditCountRule::ACCUMULATE))// 50 edits between 01 August 2019 and 31 January 2020
+            ->addRule(new HasGroupDurationRule('sysop', 90, '<202002081400'), Workflow::ON_ANY_WIKI)// flagged as a sysop for three months
+            ->withAction('<strong>be a candidate</strong>')
+            ->withExtraRequirements([
+                'You must be 18 years old, and at the age of majority in your country.',
+                'You must agree to abide by the policies governing <a href="//meta.wikimedia.org/wiki/Stewards_policy" title="Steward policy">steward access</a>, <a href="//meta.wikimedia.org/wiki/CheckUser_policy" title="checkuser policy">checkuser access</a>, <a href="//meta.wikimedia.org/wiki/Oversight_policy" title="oversight policy">oversight access</a>, and <a href="//foundation.wikimedia.org/wiki/Privacy_policy" title="privacy policy">privacy</a>.',
+                'You must <a href="https://meta.wikimedia.org/wiki/Special:MyLanguage/Access_to_nonpublic_personal_data_policy" title="Access to nonpublic personal data policy">sign the confidentiality agreement</a>.'
+            ]);
+
         ##########
         ## 2019: Commons Picture of the Year for 2018
         ##########
