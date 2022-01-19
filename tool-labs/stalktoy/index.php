0017dc2bd2d61d09bb7aea5c4749a5b14ab99d10
MarcoAurelio <30000615+MarcoAurelioWM@users.noreply.github.com>
2022-01-19 19:41:55 +0100

    Add Bullseye tool to stalktoy

--- tool-labs/stalktoy/index.php
+++ tool-labs/stalktoy/index.php
@@ -126,6 +126,7 @@ if ($engine->isValid() && $ip->ip->isValid()) {
         <div>
             Related toys:
             <a href='https://whois.toolforge.org/gateway.py?lookup=true&ip=", $ip->ip->getFriendly(), "' title='whois query'>whois</a>,
+            <a href='https://bullseye.toolforge.org/ip/", $ip->ip->getFriendly(), "' title='bullseye tool'>bullseye</a>,
             <a href='https://meta.wikimedia.org/wiki/Special:GlobalBlock?wpAddress={$engine->targetWikiUrl}' title='Special:GlobalBlock'>global block</a>.
         </div>
         ";
